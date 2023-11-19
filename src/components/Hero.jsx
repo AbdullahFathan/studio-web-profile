fcd231a42904804b53f1237a01c683dc0e1033a7
AbdullahFathan <abdullahfathan013@gmail.com>
2023-11-20 00:38:33 +0700

    style: navbar mobile version

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,10 +1,34 @@
+import { navbarConst } from "../constants/navbar";
+import { useState } from "react";
 import Navbar from "./Navbar";
 
 const Hero = () => {
+  const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
+
+  const toggleMobileMenu = () => {
+    setIsMobileMenuOpen(!isMobileMenuOpen);
+  };
   return (
     <section className="hero">
       <div className="container max-w-6xl mx-auto px-6 py-12  ">
-        <Navbar />
+        <Navbar
+          toggleMobileMenu={toggleMobileMenu}
+          isMobileMenuOpen={isMobileMenuOpen}
+        />
+        {isMobileMenuOpen && (
+          <div
+            id="menu"
+            className={`absolute top-0 bottom-0 left-0 ${
+              isMobileMenuOpen ? "flex" : "hidden"
+            } flex-col self-end  w-full min-h-screen py-1 pt-40 pl-20 space-y-3 text-lg text-white uppercase bg-black`}
+          >
+            {navbarConst.map((item, index) => (
+              <a key={index} href="#" className="hover:text-green-400">
+                {item}
+              </a>
+            ))}
+          </div>
+        )}
         <div className="max-w-lg mt-32 mb-32 p-4 font-sans text-4xl text-white uppercase border-2  md:p-10 md:m-32 md:mx-0 md:text-6xl ">
           Impressive expreince that deliver
         </div>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,10 @@
 import { navbarConst } from "../constants/navbar";
 
-const Navbar = () => {
+const Navbar = ({ toggleMobileMenu, isMobileMenuOpen }) => {
   return (
-    <nav className="flex items-center justify-between font-bold text-white">
+    <nav className="flex items-center justify-between font-bold text-white ">
       <img src="/images/logo.svg" alt="logo" />
+      {/*Desktop NavBar */}
       <div className="hidden h-10 font-alata md:flex md:space-x-8">
         {navbarConst.map((item, index) => (
           <div key={index} className="group">
@@ -11,6 +12,21 @@ const Navbar = () => {
             <div className=" mx-2 group-hover:border-b group-hover:border-blue-50" />
           </div>
         ))}
+      </div>
+      {/*Mobile NavBar */}
+      <div className="md:hidden  ">
+        <button
+          id="menu-btn"
+          type="button"
+          className={`z-40 mb-4 block hamburger md:hidden focus:outline-none ${
+            isMobileMenuOpen ? "open" : ""
+          }`}
+          onClick={toggleMobileMenu}
+        >
+          <span className="hamburger-top"></span>
+          <span className="hamburger-middle"></span>
+          <span className="hamburger-bottom"></span>
+        </button>
       </div>
     </nav>
   );
